2511fd83cd154aeeb6ee94549043a22f3e0c19a4
tiagodsilva <tdsh97@gmail.com>
2021-09-25 23:37:31 -0300

    Generate description of Revision's data set for model evaluation

---
+++ generate_data_description.py
@@ -0,0 +1,42 @@
+import os 
+import glob 
+import re 
+
+types = ["academic", "quartz", "vega"] 
+root = "data" 
+labels = ["predicted", "bbs", "mask", "debug"]   
+
+def main(): 
+    for cls in types: 
+        print("Generating data for", cls, end = "\r", flush = True) 
+        description = open(cls + ".txt", "w") 
+        data_dir = root + "/" + cls 
+
+        # for academic and quartz, the images 
+        # aren't compartimentalized in folders 
+        if cls in types[:2]: 
+            files = os.listdir(data_dir) 
+            for img in files: 
+                is_raw = ".png" in img 
+                for label in labels: 
+                    is_raw = is_raw and (label not in img) 
+
+                if is_raw: 
+                    description.write(img + "\n") 
+        else: 
+            folders = glob.glob(data_dir + "/*") 
+            for folder in folders: 
+                print("Current folder:", folder) 
+                files = os.listdir(folder) 
+                # print("files", files) 
+                for img in files: 
+                    # print(img) 
+                    is_raw = ".png" in img 
+                    for label in labels: 
+                        is_raw = is_raw and (label not in img) 
+
+                    if is_raw: 
+                        description.write(folder[10:] + "/" + img + "\n") 
+
+if __name__ == "__main__": 
+    main() 
