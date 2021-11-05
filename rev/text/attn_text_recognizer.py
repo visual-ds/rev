f71f8bd984968b12e34fefebdcaebb69ad9d6107
tiagodsilva <tdsh97@gmail.com>
2021-11-04 21:38:21 -0300

    Insert disclaimer; it contains the source of some lines we consolidated to build our pipeline

--- rev/text/attn_text_recognizer.py
+++ rev/text/attn_text_recognizer.py
@@ -13,6 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+""" 
+The following lines consolidate the files 
+available in https://github.com/clovaai/deep-text-recognition-benchmark. 
+""" 
 import numpy as np
 
 import os
--- rev/text/ocr.py
+++ rev/text/ocr.py
@@ -17,29 +17,6 @@ from . import rectutils as ru
 
 from ..textbox import TextBox
 
-# packages for deep text recognition
-import string
-import torch
-import torch.backends.cudnn as cudnn
-import torch.utils.data
-import torch.nn.functional as F
-
-import sys
-sys.path.insert(1, os.path.join(sys.path[0], ".."))
-
-from . attn_text_recognizer import (
-    AttnLabelConverter,
-    RawDataset,
-    AlignCollate,
-    TextBoxDataset,
-    Model
-)
-# from models.dlocr.utils import AttnLabelConverter
-# from models.dlocr.dataset import RawDataset, AlignCollate, TextBoxDataset
-# from models.dlocr.model import Model
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # temporal
 
 # from chartprocessor.chart import save_bbs
@@ -52,6 +29,7 @@ from PIL import Image
 
 
 SHOW = False
+
 
 def post_process_text(text):
     # '1O99' -> '1099'
@@ -105,7 +83,7 @@ def run_ocr_in_points_boxes(img, point_set, pad=0, psm=PSM.SINGLE_LINE, debug=Fa
                              fpad, cv2.BORDER_CONSTANT, value=(255, 255, 255))
     fh, fw, _ = img.shape
 
-    api = PyTessBaseAPI(psm=psm, lang='eng', path = "/usr/share/tesseract-ocr/4.00/tessdata/")
+    api = PyTessBaseAPI(psm=psm, lang='eng')
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
 
     boxes = []
@@ -126,24 +104,24 @@ def run_ocr_in_points_boxes(img, point_set, pad=0, psm=PSM.SINGLE_LINE, debug=Fa
         #u.show_image('ex', roi_bw)
 
         border_size = 3
-        roi_bw = cv2.copyMakeBorder(roi_bw,
-                 border_size,
-                 border_size,
-                 border_size,
-                 border_size,
-                 cv2.BORDER_CONSTANT,
+        roi_bw = cv2.copyMakeBorder(roi_bw, 
+                 border_size, 
+                 border_size, 
+                 border_size, 
+                 border_size, 
+                 cv2.BORDER_CONSTANT, 
                  value=255
         )
 
         #u.show_image('ex', roi_bw)
 
         pil_img = Image.fromarray(roi_bw)
-
+        
         max_conf = -np.inf
-
+        
         correct_text = ''
         correct_angle = 0
-
+        
         for angle in [0, -90, 90]:
             rot_img = pil_img.rotate(angle, expand=1)
             api.SetImage(rot_img)
@@ -237,7 +215,7 @@ def run_ocr_in_boxes(img, boxes, pad=0, psm=PSM.SINGLE_LINE, debug = False):
         for angle in [0, -90, 90]:
             rot_img = pil_img.rotate(angle, expand=1)
 
-
+            
 
             api.SetImage(rot_img)
             conf = api.MeanTextConf()
@@ -265,105 +243,6 @@ def run_ocr_in_boxes(img, boxes, pad=0, psm=PSM.SINGLE_LINE, debug = False):
     api.End()
 
     return boxes
-
-def deep_ocr(args, opt, text_boxes, chart_image, pad = 0):
-
-    if None in args.values():
-        raise KeyError(f"The parameters {list(args.keys())} should be available!")
-
-    if opt["sensitive"]:
-        opt["character"] = string.printable[:-6]
-
-    # u.show_image("ocar", chart_image)
-
-    args = {**args, **opt}
-
-    converter = AttnLabelConverter(args["character"])
-
-    args["num_class"] = len(converter.character)
-
-    if opt["rgb"]:
-        opt["input_channel"] = 3
-
-    model = Model(args)
-
-    print("Model input parameters", args, opt)
-
-    model = torch.nn.DataParallel(model).to(device)
-
-    # load model
-    print("loading pretrained model from", args["saved_model"])
-
-    model.load_state_dict(torch.load(args["saved_model"], map_location = device))
-
-    # prepare data
-    align = AlignCollate(imgH = args["imgH"], imgW = args["imgW"], keep_ratio_with_pad = args["PAD"])
-    # data = RawDataset(root = args["image_folder"], opt = args)
-    data = TextBoxDataset(chart_image, text_boxes, args, pad = pad)
-    loader = torch.utils.data.DataLoader(
-        data, batch_size = args["batch_size"],
-        shuffle = False,
-        num_workers = int(args["workers"]),
-        collate_fn = align, pin_memory = True
-    )
-
-    # predict
-    model.eval()
-    with torch.no_grad():
-        for image_tensors, image_path_list in loader:
-            # print(image_tensors)
-            batch_size = image_tensors.size(0)
-            image = image_tensors.to(device)
-            length_for_pred = torch.IntTensor([opt["batch_max_length"]] * batch_size).to(device)
-            text_for_pred = torch.LongTensor(batch_size, args["batch_max_length"] + 1).fill_(0).to(device)
-
-        # u.show_image(image, "a")
-        preds = model(image, text_for_pred, is_train = False)
-        _, preds_index = preds.max(2)
-        preds_str = converter.decode(preds_index, length_for_pred)
-
-        log = str()
-        preds_prob = F.softmax(preds, dim = 2)
-        preds_max_prob, _ = preds_prob.max(dim = 2)
-
-        # boxes = dict()
-
-        for box in text_boxes:
-            box._text_conf = -np.inf
-
-        angles = data.angles
-
-        for img_name, pred, pred_max_prob in zip(image_path_list, preds_str, preds_max_prob):
-            pred_EOS = pred.find("[s]")
-            pred = pred[:pred_EOS] # [s] is the end of sentence token
-            pred_max_prob = pred_max_prob[:pred_EOS]
-            # print(img_name, pred)
-
-            box_idx, angle = list(map(int, img_name.split("|")))
-
-            box = text_boxes[box_idx]
-
-            confidence_score = pred_max_prob.cumprod(dim = 0)[-1]
-
-            # angle = angles[angle]
-
-            if confidence_score * 100 > box._text_conf:
-                box._text = pred
-                box._text_conf = confidence_score * 100
-                box._text_dist = 1e-9
-                box._text_angle = angle
-                # print(box._text, box._text_conf)
-
-            # print(box._text, box._text_conf)
-            # print(pred, confidence_score)
-            # dashed = "~" * 99
-            # print(dashed)
-
-            log = log + f"{img_name:25s}\t{pred:25s}\t{confidence_score:.4f}"
-    # print(log)
-    torch.cuda.empty_cache()
-    return text_boxes
-
 
 
 # def run_ocr_in_chart(chart, from_bbs, pad=0):
